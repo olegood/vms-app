fb7babc240d5ff735a38428788fcc98c685d5982
Oleg Anastassov <oleg.anastassov@gmail.com>
2024-07-21 17:35:11 +0200

    add jib plugin and sample data to test ui

---
+++ src/main/java/vms/api/VoucherController.java
@@ -0,0 +1,27 @@
+package vms.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/voucher")
+public class VoucherController {
+
+    record Voucher(UUID id, String organization, int count) {
+    }
+
+    @GetMapping
+    public Collection<Voucher> getAllVouchers() {
+        return List.of(
+                new Voucher(UUID.randomUUID(), "GE", 15),
+                new Voucher(UUID.randomUUID(), "ABC Nova", 8),
+                new Voucher(UUID.randomUUID(), "Sewerth", 4)
+        );
+    }
+
+}
