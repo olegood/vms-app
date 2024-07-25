bc245342ffb6d1d39f3e8137e9859a54821f206d
Oleg Anastassov <oleg.anastassov@gmail.com>
2024-07-25 17:59:21 +0200

    add explicit table name

--- src/main/java/vms/domain/Voucher.java
+++ src/main/java/vms/domain/Voucher.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.io.Serializable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @Entity
+@Table(name = "voucher")
 public class Voucher implements Serializable {
 
   @Id
