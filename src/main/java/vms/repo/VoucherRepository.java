b32d9a4d8b9e349b9861038e85dacdb4a45248e0
Oleg Anastassov <oleg.anastassov@gmail.com>
2024-07-29 01:24:28 +0300

    jacoco configuration (#1)

--- src/main/java/vms/repo/VoucherRepository.java
+++
@@ -1,6 +0,0 @@
-package vms.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vms.domain.Voucher;
-
-public interface VoucherRepository extends JpaRepository<Voucher, Long> {}
